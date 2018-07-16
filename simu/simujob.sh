39196db794ca633f2ac62bb82ad6088082c017b2
Pere Jaakko <perej1@inspiraatio.org.aalto.fi>
2018-07-16 16:39:28 +0300

    triton simut

---
+++ simu/simujob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=0-00:10:00             # 5 mins
+#SBATCH --mem-per-cpu=500             # 500MB of memory
+#SBATCH --array=100-300:100           # Array from 100 to 200 with 10 jumps
+#SBATCH --partition=debug             # Partition
+#SBATCH --job-name=simujob            # Name of the job
+#SBATCH --output=./output/simujob.%j.out
+
+# If modules aren't loaded, load them.
+# module load python
+# module load numpy
+# module load scipy
+
+cd $WRKDIR
+ki=$(srun python ksqrt.py -n $SLURM_ARRAY_TASK_ID)
+srun python hillSim.py -n $SLURM_ARRAY_TASK_ID -r 10 -k `echo ${ki}` -f ./data -d pareto
